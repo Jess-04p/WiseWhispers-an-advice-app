5f1cc8b230c096481e57f8e3f4055c23f8ac3a14
Jess-04p <132880894+Jess-04p@users.noreply.github.com>
2024-02-25 23:44:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state={advices:''};
+
+    componentDidMount(){
+        this.fetchAdv();
+    }
+
+    fetchAdv = () =>{
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+            const {advice}=response.data.slip;
+            this.setState({advice});
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    handleCopy = () => {
+        const { advice } = this.state;
+        navigator.clipboard.writeText(advice)
+          .then(() => alert('Advice copied to clipboard!'))
+          .catch(error => console.error('Unable to copy advice:', error));
+    };
+
+    render(){
+        const {advice}=this.state;
+        return(
+            <div className="section">
+                <div className="card">
+                    <h1 className="mainheading">WORDS OF WISDOM</h1>
+                    <h1 className="headingg">{advice}</h1>
+                    <div className="button-container">
+                    <button className="buttonn1" onClick={this.fetchAdv}>
+                        <span>NEXT ADVICE</span>
+                    </button>
+                    <button className="buttonn2" onClick={this.handleCopy}>
+                        <span>COPY</span>
+                    </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
